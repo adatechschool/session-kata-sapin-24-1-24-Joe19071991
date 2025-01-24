b05c77a4c9ab2168ee17d1749840621ffb54377b
Joe19071991 <lauranove66@gmail.com>
2025-01-24 10:23:40 +0100

    correction étape 1

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// const afficherPointSapin = (hauteur) => {};
-// afficherPointSapin(4);
+// étape 1.1 afficher étoiles :
+
 const afficherEtoiles = (n) => {
   let etoile = "";
-  for (let i = 1; i <= n; i++) {
-    etoile += "*";
-    console.log(etoile);
-  }
+  for (let i = 1; i <= n; i++) etoile += "*";
+  console.log(etoile);
 };
-
-afficherEtoiles(2);
-afficherEtoiles(5);
+afficherEtoiles(3);
+afficherEtoiles(6);
